290dda6b8d9003a3c187bac031e9b204e824b673
Lucas <lucasleveque17@gmail.com>
2025-03-31 16:35:46 +0200

    Modification des motifs d'ignorance dans la configuration Jest pour utiliser des chemins Unix, remplaçant les doubles barres obliques inverses par des barres obliques simples.

--- jest.config.ts
+++ jest.config.ts
@@ -162,10 +162,9 @@ const config: Config = {
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: [
-    "\\\\node_modules\\\\",
-    "\\\\tests\\\\e2e\\\\"
+    "/node_modules/",
+    "/tests/e2e/"
   ],
-
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
